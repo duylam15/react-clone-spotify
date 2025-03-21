a54bcf384752e7d810bd9bd50e7ce77da97dbc9f
lamkbv <lamkbvn@gmail.com>
2025-03-21 11:35:28 +0700

    no thing

--- src/pages/search.tsx
+++ src/pages/search.tsx
@@ -1,3 +1,3 @@
 export default function Search(): React.ReactNode {
-  return <div className="flex w-full bg-yellow-500 p-2">Search</div>;
+  return <div className="flex w-full bg-yellow-500 p-2">Search abc</div>;
 }
