8d93d6aaa40bd945623e55cfa53efb8b7b4ec27c
LocNguyenSGU <lockbkbang@gmail.com>
2025-04-10 10:14:48 +0700

    feat: crud bai hat admin

--- src/services/songAPI.ts
+++ src/services/songAPI.ts
@@ -12,3 +12,51 @@ export const getSongs = async (): Promise<Song[]> => {
       throw error;
     }
 }
+
+
+export const getBaiHat_PhanTrang = async (page: number, size: number, search: string) => {
+    try {
+        const response = await axios.get(`${API_BASE_URL}/baihat/api/pagination/?page=${page}&size=${size}&search=${search}`);
+        return response.data;
+    } catch (error) {
+        console.error("Error fetching nghe si", error);
+        throw error;
+    }
+};
+
+export const addBaiHat = async (formData: FormData) => {
+  return axios.post(`${API_BASE_URL}/baihat/baihat/them/`, formData, {
+    headers: {
+      "Content-Type": "multipart/form-data",
+    },
+  });
+};
+
+
+export const updateBaiHat = async (formData: FormData, id: number) => {
+  return axios.put(`${API_BASE_URL}/baihat/baihat/capnhat/${id}/`, formData, {
+    headers: {
+      "Content-Type": "multipart/form-data",
+    },
+  });
+};
+
+export const lockBaiHat = async (idBaiHat: number) => {
+  try {
+      const response = await axios.patch(`${API_BASE_URL}/api/nghesi/lock/${idBaiHat}`);
+      return response.data;
+  } catch (error) {
+      console.error("Error locking nghe si", error);
+      throw error;
+  }
+};
+
+export const unlockBiaHat = async (idBaiHat: number) => {
+  try {
+      const response = await axios.patch(`${API_BASE_URL}/api/nghesi/unlock/${idBaiHat}`);
+      return response.data;
+  } catch (error) {
+      console.error("Error unlocking nghe si", error);
+      throw error;
+  }
+};
